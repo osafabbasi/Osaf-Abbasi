e77a35ceac3e18967cb351424d0c60bee3e92ad0
osafabbasi <osafabbasi242@gmail.com>
2026-03-14 17:55:29 -0700

    completed about me in sectwo

--- src/components/Sectwo.jsx
+++ src/components/Sectwo.jsx
@@ -11,15 +11,26 @@ function Sectwo() {
         </p>
       </div>
 
-      <div className='h-160 w-full '>
-        <div className='h-full w-full bg-cover flex p-4 items-center justify-evenly bg-center' style={{ backgroundImage: `url(${bg})` }}>
-          <div className='h-11/12  w-1/4  rounded-2xl p-2'></div>
-          <div className='h-11/12  w-1/3 space-y-3  rounded-2xl p-2 sm:p-6 md:p-8'>
-            <h1 className='text-xl sm:text-3xl md:text-4xl classic-text ]'><span className='border-2 text-[#f1d2ab] font-bold p-2 rounded-4xl '>Yello There </span></h1><br /><h1 className='text-lg sm:text-4xl cool-text'>I am Muhammad <span className='text-[#cf944b] font-black'>Osaf Abbasi</span> </h1>
-          </div>
-          <div className='h-11/12  w-1/3  rounded-2xl p-2'></div>
-        </div>
+      <div className='min-h-160 w-full '>
+        <div className=' h-full w-full bg-cover bg-center' style={{ backgroundImage: `url(${bg})` }}>
+          <div className='h-full w-full flex p-4 items-center justify-evenly '>
 
+            <div className='h-11/12  w-1/4  rounded-2xl p-2'></div>
+            <div className='h-11/12  w-1/2 sm:w-1/3 space-y-3  rounded-2xl p-2 sm:p-6 md:p-8'>
+              <h1 className='text-sm sm:text-3xl md:text-2xl cool-text '><span className='border-2 text-[#f1d2ab] font-normal p-2 rounded-4xl '>Yello There </span></h1><br /><h1 className='text-lg sm:text-4xl cool-text'>I am Muhammad <span className='text-[#cf944b] font-black'>Osaf Abbasi</span> </h1> <br />
+              <p className='cool-text text-[#F3E9DC] font-normal text-[70%] sm:text-sm tracking-tight sm:tracking-wide'>I’m a passionate 2D Digital Artist and Frontend Developer who enjoys creating engaging and creative digital experiences. I love bringing ideas to life through my artwork, animation, and interactive design. Driven by creativity, I explore different ways to blend visual storytelling with modern web experiences. Currently based in Karachi.</p>
+            </div>
+            <div className='h-11/12  hidden sm:flex  w-1/3  rounded-2xl p-2 '>
+            <div className='flex h-full w-full  '>
+             <h1 className='cool-text'>EXPERIENCE</h1>
+             
+             
+             </div>
+            </div>
+
+          </div>
+          <div className='h-60 w-full block sm:hidden '></div>
+        </div>
       </div>
 
     </>
